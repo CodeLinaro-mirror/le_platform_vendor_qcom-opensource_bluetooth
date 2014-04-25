d2d799af4777e4a08c4d285de6d9c7eb87ad4b93
Smriti Gupta <smritig@codeaurora.org>
2014-04-25 15:18:51 +0530

    SAP: Handle the case of remote side DISC properly
    If the SAP is getting disconnect from the remote side
    but the sapDevice is already NULL, UI status was not
    updated properly as DISCONNECTED in the DUT.
    
    Change-Id: I2ff0882fb0c871cfabc7a5be41e54ae0583080cb
    CRs-Fixed: 643437

--- src/org/codeaurora/bluetooth/sap/BluetoothSapService.java
+++ src/org/codeaurora/bluetooth/sap/BluetoothSapService.java
@@ -73,8 +73,8 @@ import org.codeaurora.bluetooth.R;
 public class BluetoothSapService extends Service {
 
     private static final String TAG                             = "BluetoothSapService";
-    private static final boolean DBG                            = false;
-    public  static final boolean VERBOSE                        = false;
+    private static final boolean DBG                            = true;
+    public  static final boolean VERBOSE                        = true;
 
     private final static String SAP_SERVER                      = "qcom.sap.server";
 
@@ -1049,8 +1049,6 @@ public class BluetoothSapService extends Service {
             closeSapdSocket();
             /* close the rfcomm socket */
             closeRfcommSocket();
-            // Intimate the Settings APP about the disconnection
-            handleSapDeviceStateChange(mRemoteDevice, BluetoothProfile.STATE_DISCONNECTED);
 
             /* wait for sap downlink thread to close */
             synchronized (mDownlinkLock) {
@@ -1064,6 +1062,10 @@ public class BluetoothSapService extends Service {
                     }
                 }
             }
+
+            // Intimate the Settings APP about the disconnection
+            handleSapDeviceStateChange(mRemoteDevice, BluetoothProfile.STATE_DISCONNECTED);
+
             if (IntExit && !stopped) {
                 if (VERBOSE) Log.v(TAG, "starting the listener thread ");
                 /* start the listener thread */
@@ -1314,14 +1316,15 @@ public class BluetoothSapService extends Service {
             prevState = sapDevice.mState;
         }
         Log.d(TAG, "handleSapDeviceStateChange preState: " + prevState + " state: " + state);
-        if (prevState == state) return;
 
-        if (sapDevice == null) {
-            sapDevice = new BluetoothSapDevice(state);
-            mSapDevices.put(device, sapDevice);
-        } else {
+        if (prevState != state) {
+            if (sapDevice == null) {
+                sapDevice = new BluetoothSapDevice(state);
+                mSapDevices.put(device, sapDevice);
+            } else {
             sapDevice.mState = state;
-        }
+            }
+       }
 
         /* Notifying the connection state change of the profile before sending
            the intent for connection state change, as it was causing a race
