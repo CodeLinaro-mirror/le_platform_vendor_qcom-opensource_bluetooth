e4b51d85abb6fa6ff3f774bb288b4ec25a46bfe1
Hemant Gupta <hemantg@codeaurora.org>
2014-12-16 18:17:38 +0530

    Revert "Bluetooth-wipower: Flag based A4WP enabling."
    This reverts commit 51556e9033a89a92518857caa642890226d1bde3 which
    prevent BT crash on PL's where BOARD_USES_WIPOWER flag was set as
    FALSE.
    
    Change-Id: Ica19a086cbbb59261ace07fe8d927ace97eda8d8
    CRs-Fixed: 771812

--- Android.mk
+++ Android.mk
@@ -8,11 +8,8 @@ src_dirs:= src/org/codeaurora/bluetooth/btcservice \
            src/org/codeaurora/bluetooth/ftp \
            src/org/codeaurora/bluetooth/dun \
            src/org/codeaurora/bluetooth/sap \
-           src/org/codeaurora/bluetooth/pxpservice
-
-ifeq ($(BOARD_USES_WIPOWER),true)
-           src_dirs += src/org/codeaurora/bluetooth/a4wp
-endif
+           src/org/codeaurora/bluetooth/pxpservice \
+           src/org/codeaurora/bluetooth/a4wp
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, $(src_dirs)) \
@@ -23,9 +20,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_JAVA_LIBRARIES := javax.obex
 LOCAL_JAVA_LIBRARIES += mms-common
 LOCAL_JAVA_LIBRARIES += telephony-common
-ifeq ($(BOARD_USES_WIPOWER),true)
 LOCAL_JAVA_LIBRARIES += com.quicinc.wbc
-endif
 
 LOCAL_STATIC_JAVA_LIBRARIES := com.android.vcard
 
