a7856c7a52b2f591f607820293f8693e7a4ab859
gaoxiang <gaoxiang@codeaurora.org>
2017-04-14 16:46:03 +0800

    A2DP V1.3: Add a new callback function into src vendor interface. app should use this callback to recieve the delay report notification from SNK side.
    Change-Id: I5bd144bc0adcead89499709a8fe1c41aa266548c
    CRs-fixed: 2006292

--- vhal/include/hardware/bt_av_vendor.h
+++ vhal/include/hardware/bt_av_vendor.h
@@ -187,6 +187,7 @@ typedef void (* btav_is_multicast_enabled_vendor_callback)(int state);
  */
 typedef void (* btav_audio_focus_request_vendor_callback)(bt_bdaddr_t *bd_addr);
 
+typedef void (* btav_delay_report_vendor_callback)(bt_bdaddr_t *bd_addr, uint16_t report_delay);
 /** BT-AV Vendor callback structure. */
 typedef struct {
     /** set to sizeof(btav_vendor_callbacks_t) */
@@ -194,6 +195,7 @@ typedef struct {
     btav_connection_priority_vendor_callback connection_priority_vendor_cb;
     btav_is_multicast_enabled_vendor_callback multicast_state_vendor_cb;
     btav_audio_focus_request_vendor_callback audio_focus_request_vendor_cb;
+    btav_delay_report_vendor_callback delay_report_vendor_cb;
 } btav_vendor_callbacks_t;
 
 typedef struct {
