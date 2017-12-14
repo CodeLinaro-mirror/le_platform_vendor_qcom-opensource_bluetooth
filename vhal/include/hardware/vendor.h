d0cb6390167235ec82732baeeb46a16861826414
Apurva Srivastava <apursriv@codeaurora.org>
2017-12-14 11:01:40 +0530

    BLE : Add interface for updating LE name.
    New API that helps in setting LE BT Name.
    
    CRs-Fixed: 2156268
    Change-Id: Ia72195bad7619903f573ed6ba061a7ca0c7ad378

--- vhal/include/hardware/vendor.h
+++ vhal/include/hardware/vendor.h
@@ -25,6 +25,16 @@ __BEGIN_DECLS
 #define BT_PROFILE_VENDOR_ID "vendor"
 #define BT_PROFILE_WIPOWER_VENDOR_ID "wipower"
 
+/** Bluetooth Device Name */
+typedef struct {
+    uint8_t name[249];
+} __attribute__((packed))bt_lename_t;
+
+typedef struct{
+    int len;
+    void *val;
+}btvendor_lename_t;
+
 /** Callback when bredr cleanup is done.
  */
 typedef void (*  btvendor_bredr_cleanup_callback)(bool status);
@@ -69,6 +79,8 @@ typedef struct {
     void  (*cleanup)( void );
 
     bool (*interop_db_match)( int feature, int type, void *value);
+
+    void (*setLeBtName)(btvendor_lename_t* name);
 
 } btvendor_interface_t;
 
