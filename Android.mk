40faac1efa306060365105801145cc2b5fc897a8
Ying Wu <yingwu@codeaurora.org>
2018-12-05 14:05:58 +0800

    BTTestApp: Propagate changes from bt-qc.lnx.2.0.1
    Due to different Bluetooth API supported in Android framework,
    the features below are excluded temporarily.
    
    (1) AVRCP
    API: startFetchingAlbumArt
    
    (2) DIP(Client)
    
    (3) MAP(MCE)
    
    (4) PBAP(PCE)
    API: CUSTOM_ACTION_XXX
    
    Change-Id: Id988c6684f170a447f90ac530e9fa88526e8c130
    Signed-off-by: Ying Wu <yingwu@codeaurora.org>

--- Android.mk
+++ Android.mk
@@ -5,9 +5,11 @@ TMP_LOCAL_PATH := $(LOCAL_PATH)
 
 
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-include $(TMP_LOCAL_PATH)/bt_logger/Android.mk
-include $(TMP_LOCAL_PATH)/libbt-logClient/Android.mk
-include $(TMP_LOCAL_PATH)/BluetoothExt/Android.mk
+#include $(TMP_LOCAL_PATH)/wipower-host/Android.mk
+#include $(TMP_LOCAL_PATH)/bt_logger/Android.mk
+#include $(TMP_LOCAL_PATH)/libbt-logClient/Android.mk
+#include $(TMP_LOCAL_PATH)/BluetoothExt/Android.mk
+#include $(TMP_LOCAL_PATH)/hidtestapp/Android.mk
 include $(TMP_LOCAL_PATH)/bttestapp/Android.mk
 endif #TARGET_USE_QTI_BT_STACK
 
