88fcee678a381e907119c6c6060dc1dcb21563ba
auniyal <auniyal@codeaurora.org>
2021-05-25 14:06:08 +0530

    Minor Change in Input Parse. Changes include -   -- Replacing the delimiter from space to comma for multiple permission and services.   -- changes in Create Service.
    Change-Id: I1162663a4ed2c6ef513e415ea3cb83fe79068039

--- testApp/src/org/codeaurora/bluetooth/testApp/InputParse.java
+++ testApp/src/org/codeaurora/bluetooth/testApp/InputParse.java
@@ -372,18 +372,14 @@ public class InputParse {
                  if (tmp2[0].equals("ServiceUuid")) {
                       AddServicesParam.lserviceUUID = UUID.fromString(tmp2[1]);
                  } else if (tmp2[0].equals("CharUuid")) {
-                      String tmp3[] = tmp2[1].split(" ");
-                      for (int index=0; index<tmp3.length; index++){
-                        AddServicesParam.lcharUUIDs=new ArrayList<>();
-                        AddServicesParam.lcharUUIDs.add(UUID.fromString(tmp3[index]));
-                      }
+                      AddServicesParam.lcharUUID = UUID.fromString(tmp2[1]);
                  } else if (tmp2[0].equals("Properties")) {
-                      String tmp3[] = tmp2[1].split(" ");
+                      String tmp3[] = tmp2[1].split(",");
                       List<String> list = Arrays.asList(tmp3);
                       AddServicesParam.lProps = list.stream().map(Integer::decode)
                             .collect(Collectors.toList());
                  } else if (tmp2[0].equals("Permissions")) {
-                      String tmp3[] = tmp2[1].split(" ");
+                      String tmp3[] = tmp2[1].split(",");
                       List<String> list = Arrays.asList(tmp3);
                       AddServicesParam.lPerms = list.stream().map(Integer::decode)
                             .collect(Collectors.toList());
