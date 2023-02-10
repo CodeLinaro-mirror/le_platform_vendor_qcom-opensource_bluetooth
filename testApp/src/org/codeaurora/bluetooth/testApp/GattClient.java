b7804d30285990dba74889ffa906e4172e7799a7
Prathibha Madugonde <quic_prathm@quicinc.com>
2023-02-10 11:06:09 +0530

    GattClient : Closing GattClient when onDestroy() is called
    Change-Id: Iaa07dbc6fd7595e1dcd4f9890f8ef6d7f276edca

--- testApp/src/org/codeaurora/bluetooth/testApp/GattClient.java
+++ testApp/src/org/codeaurora/bluetooth/testApp/GattClient.java
@@ -180,6 +180,7 @@ public class GattClient {
         }
         /* stop the looper */
         glooper.quitSafely();
+        mGattClientHandler.processCloseReq();
     }
 
     /* Connection Class */
@@ -828,6 +829,11 @@ public class GattClient {
             Log.i(TAG, "Disconnecting!");
             mConnectionStatus = BLE_STATE_DISCONNECTING;
             mgattClient.disconnect();
+        }
+
+        private void processCloseReq() {
+            Log.i(TAG, "Close!");
+            mgattClient.mBluetoothGatt.close();
         }
 
         private void processPhyUpdateReq(PhyUpdate phyUpdate){
