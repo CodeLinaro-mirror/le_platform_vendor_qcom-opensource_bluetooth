e0706b43480c488f5451424f737bf99659aa800f
Naga Bhavani Akella <quic_nakella@quicinc.com>
2023-10-06 09:55:00 +0530

    Minor changes in Advertiser Service
    Changes include -
     -- Implemeting changes in the indexing of Advertising sets
     -- Removing unnecessary functions in adv module
     -- Re-arranging prints on console for adv module
    
    Change-Id: I81f698869d3a720f7ec4cdc73d145a02c805a1e3

--- testApp/src/org/codeaurora/bluetooth/testApp/AdvertiserService.java
+++ testApp/src/org/codeaurora/bluetooth/testApp/AdvertiserService.java
@@ -34,6 +34,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+import android.os.Message;
 import java.util.ArrayList;
 
 public class AdvertiserService extends Service {
@@ -41,6 +42,9 @@ public class AdvertiserService extends Service {
     private static String TAG = "ADV SERVICE";
     private ArrayList<AdvertiserEntity> advertisements;
     private static final int MAX_ADVERTISEMENTS = 16;
+    private int[] removed_indices = new int[MAX_ADVERTISEMENTS];
+    private static int INVALID_INDEX = -1;
+    private static int removed_count = INVALID_INDEX;
     private AdvServiceCallback mAdvServiceCb;
 
     public class LocalBinder extends Binder {
@@ -81,7 +85,7 @@ public class AdvertiserService extends Service {
         Log.d(TAG,"onDestroy");
         for(int i = 0; i < advertisements.size(); i++) {
             AdvertiserEntity advInstance = advertisements.get(i);
-            if(advInstance.adv_status == AdvertiserEntity.ADV_STARTED) {
+            if(advInstance.getAdv_status() == AdvertiserEntity.ADV_STARTED) {
                 advInstance.StopAdvertisement();
             }
         }
@@ -101,47 +105,102 @@ public class AdvertiserService extends Service {
         super.onTaskRemoved(rootIntent);
     }
 
+    public int getAdvertisingIndex() {
+        int i;
+        int temp_index = 0;
+        for(i=0; i< MAX_ADVERTISEMENTS; i++) {
+            if(advertisements.size() > 0){
+                if(removed_count != INVALID_INDEX) {
+                    temp_index = removed_indices[removed_count];
+                    removed_count --;
+                    break;
+                } else {
+                    temp_index = advertisements.size();
+                    break;
+                }
+            }
+            else {
+                temp_index = 0;
+                break;
+            }
+        }
+        return temp_index;
+    }
+
     public int startAdvertising(Adv adv_info){
         boolean status = false;
-        int index = advertisements.size();
+        int index = getAdvertisingIndex();
+
         //since array index starts a 0, size is used for next index
         if(index >= MAX_ADVERTISEMENTS){
             Log.e(TAG,"Max advertisements limit reached, abort");
+            StringBuilder PrintStr = new StringBuilder();
+            PrintStr.setLength(0);
+            PrintStr.append("Max advertisements limit reached!");
+            SocketServer.sendSocketData(PrintStr.toString());
             return MAX_ADVERTISEMENTS;
         }
         AdvertiserEntity advInstance = new AdvertiserEntity(adv_info,index, mAdvServiceCb);
-        advertisements.add(advInstance);
+        advertisements.add(index,advInstance);
         status = advInstance.StartAdvertisement();
         Log.d(TAG,"new Adv instance is created, total advs : " + advertisements.size());
         return index;
     }
 
+    public int getAdvInfoArrayIndex(int Adv_id) {
+        for(int i = 0; i < advertisements.size(); i++) {
+            AdvertiserEntity advEnt = advertisements.get(i);
+            if(advEnt.getAdv_id() == Adv_id) {
+                Log.d(TAG, "found index to be deleted,i="+i);
+                return i;
+            }
+        }
+        return INVALID_INDEX;
+    }
+
     public boolean stopAdvertising(int Adv_id) {
         boolean status = false;
+        Log.d(TAG,"Adv id"+ Adv_id + "is deleted");
         if(Adv_id >= MAX_ADVERTISEMENTS){
             Log.e(TAG,"stopAdvertising invalid adv_id");
             return status;
         }
-        AdvertiserEntity advInstance = advertisements.get(Adv_id);
-        status = advInstance.StopAdvertisement();
+        int adv_index = getAdvInfoArrayIndex(Adv_id);
+        if(INVALID_INDEX != adv_index){
+            AdvertiserEntity advInstance = advertisements.get(adv_index);
+            status = advInstance.StopAdvertisement();
+        }
+        else {
+            Log.e(TAG, "Adv Index not found");
+        }
         return status;
     }
 
     public class AdvServiceCallback {
+        Message msg;
         public void onAdvStarted(int adv_id){
             Log.d(TAG,"Advertising Started on adv_id : " + adv_id);
+            msg = BleAppService.msghandler.obtainMessage(
+                      BleAppService.MSG_MA_ADV_STARTED, Integer.toString(adv_id));
+            BleAppService.msghandler.sendMessage(msg);
         }
         public void onAdvStopped(int adv_id){
             Log.d(TAG,"Advertising Stopped on adv_id : " + adv_id +
                     "Deleting the adv instance");
-            advertisements.remove(adv_id);
-            Log.d(TAG,"Total adv instances after deleting: " + advertisements.size());
-            StringBuilder PrintStr = new StringBuilder();
+            int adv_index = getAdvInfoArrayIndex(adv_id);
+            if(INVALID_INDEX != adv_index) {
+                advertisements.remove(adv_index);
+                Log.d(TAG,"Total adv instances after deleting: " + advertisements.size());
+                removed_count++;
+                removed_indices[removed_count] = adv_index;
 
-            PrintStr.setLength(0);
-            PrintStr.append("Advertising Stopped on adv_id: ");
-            PrintStr.append(adv_id);
-            SocketServer.sendSocketData(PrintStr.toString());
+                msg = BleAppService.msghandler.obtainMessage(
+                          BleAppService.MSG_MA_ADV_STOPPED, Integer.toString(adv_index));
+                BleAppService.msghandler.sendMessage(msg);
+            }
+            else {
+                Log.e(TAG, "Adv Index not found");
+            }
         }
     }
 }
