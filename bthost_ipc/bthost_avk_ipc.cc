6d0ec19baf5b2ce9a8ab9cdd2dbe13c0580c60e9
Yepuri Siddu <quic_ysiddu@quicinc.com>
2025-03-17 13:06:33 +0530

    fix compiling issue of strlcpy in scarthgap upgrade
    Change-Id: I1d3a10d37fadfccb9604313fd4a6b25e289b2da9

--- bthost_ipc/bthost_avk_ipc.cc
+++ bthost_ipc/bthost_avk_ipc.cc
@@ -57,7 +57,7 @@
 
 #ifdef USE_GLIB
 #include <glib.h>
-#define strlcpy g_strlcpy
+//#define strlcpy g_strlcpy
 #endif
 
 /*****************************************************************************
